1ba52a68cd877bb2d9135b95e32d602503a6a835
claudiopacha <claudiomonteiro@protonmail.com>
2020-09-06 06:58:07 -0300

    update

--- lib/knn.py
+++ lib/knn.py
@@ -20,7 +20,8 @@ def KnnViewModelling(train_data, test_data, K):
         combine probabilities of models for the final
         decision, return accuracy of model  
     '''
-
+    
+    
     # split features and target
     features_train = train_data.drop(['target', 'kfold'], axis=1)
     target_train = train_data['target']
@@ -29,15 +30,17 @@ def KnnViewModelling(train_data, test_data, K):
 
     # build models for each view and return probabilities
     from sklearn.neighbors import KNeighborsClassifier
-    knn_view1 = KNeighborsClassifier(n_neighbors=K, weights='distance')
+    from sklearn.neighbors import DistanceMetric
+
+    knn_view1 = KNeighborsClassifier(n_neighbors=K, weights='distance', metric= DistanceMetric.get_metric('euclidean'))
     knn_view1.fit(select_view(features_train, 'fac'), target_train)
     pred_knn_view1 = list(knn_view1.predict_proba(select_view(features_test,  'fac')))
 
-    knn_view2 = KNeighborsClassifier(n_neighbors=K, weights='distance')
+    knn_view2 = KNeighborsClassifier(n_neighbors=K, weights='distance', metric= DistanceMetric.get_metric('euclidean'))
     knn_view2.fit(select_view(features_train, 'fou'), target_train)
     pred_knn_view2 = list(knn_view2.predict_proba(select_view(features_test,  'fou')))
 
-    knn_view3 = KNeighborsClassifier(n_neighbors=K, weights='distance')
+    knn_view3 = KNeighborsClassifier(n_neighbors=K, weights='distance', metric= DistanceMetric.get_metric('euclidean'))
     knn_view3.fit(select_view(features_train, 'kar'), target_train)
     pred_knn_view3 = list(knn_view3.predict_proba(select_view(features_test,  'kar')))
 
